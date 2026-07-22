ef0c7dbc16b49234c3285af2f999ede84b95e8f2
DarkMaster2610 <11249a016@kanchiuniv.ac.in>
2026-07-22 10:24:26 +0530

    Add files via upload

---
+++ Exp1a.java
@@ -0,0 +1,17 @@
+public class Exp1a{
+    int instanceVar=10;
+    static String staticVar= "I am Static";
+    public void show_variables()
+    {
+        int localVar=5;
+        System.out.println("Instance variable :" + instanceVar);
+        System.out.println("Static variable :" + staticVar);
+        System.out.println("Local variable :" + localVar);
+    }
+    public static void main(String[]args)
+{
+    Exp1a obj1= new Exp1a();
+    obj1.show_variables();
+    System.out.println("Accessing static variable via class:" + Exp1a.staticVar);
+}
+}
---
+++ Exp1b.java
@@ -0,0 +1,14 @@
+public class Exp1b {
+    public static void main(String[]args)
+    
+    {
+int a=21,b=10;
+System.out.println("A+B=" + (a+b));
+System.out.println("A-B=" + (a-b));
+System.out.println("a>b?" + (a>b));
+boolean x=true,y=false;
+System.out.println("x and y:"+ (x&&y));
+a+=5;
+System.out.println("A incremented by 5: " + a);
+    }
+}
