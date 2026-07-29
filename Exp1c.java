5c9d0a248ce30e01e357112558ed9c0f81b810d5
DarkMaster2610 <11249a016@kanchiuniv.ac.in>
2026-07-29 11:18:47 +0530

    Add files via upload

---
+++ Exp1c.java
@@ -0,0 +1,16 @@
+//Reference to an Instance of a Class (Object) and Methods
+class Exp1c {
+String name = "Krishna";
+int age = 20;
+
+void display() {
+System.out.println("Name: " + name);
+System.out.println("Age: " + age);
+}
+
+public static void main(String[] args) {
+Exp1c s = new Exp1c();
+s.display();
+}
+}
+    
