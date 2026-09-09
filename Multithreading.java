16d030fd3e7bad9232083dc867643e0b43ecbcc4
DarkMaster2610 <11249a016@kanchiuniv.ac.in>
2026-09-08 23:25:03 -0800

    Add files via upload

---
+++ Multithreading.java
@@ -0,0 +1,22 @@
+// Multithreading.java
+class Thread1 extends Thread {
+    public void run() {
+        System.out.println("Thread 1 is running");
+    }
+}
+
+class Thread2 extends Thread {
+    public void run() {
+        System.out.println("Thread 2 is running");
+    }
+}
+
+public class Multithreading {
+    public static void main(String[] args) {
+        Thread1 t1 = new Thread1();
+        Thread2 t2 = new Thread2();
+
+        t1.start();
+        t2.start();
+    }
+}
---
+++ PackageDemo.java
@@ -0,0 +1,8 @@
+import mypackage.Message;
+
+public class PackageDemo {
+    public static void main(String[] args) {
+        Message m = new Message();
+        m.display();
+    }
+}
